d8168ca514625d86ba6b125a31a58d76278ed0a9
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2023-07-27 02:12:00 -0400

    Add more gnome extensions.

--- installers/fedora.sh
+++ installers/fedora.sh
@@ -15,7 +15,7 @@ sudo cp -r ../shortcuts/* /usr/share/applications/
 # Install dnf packages
 echo "Installing dnf packages."
 sudo dnf update -y
-sudo dnf install akmod-nvidia alien audacity dconf-editor deja-dup ffmpeg-free gcc gcc-c++ gimp gnome-extensions-app gnome-shell-extension-appindicator gnome-shell-extension-caffeine gnome-shell-extension-dash-to-dock gnome-tweaks htop java-17-openjdk-* mpv ncdu neofetch nmap nodejs nvtop obs-studio steam yt-dlp -y
+sudo dnf install akmod-nvidia alien audacity dconf-editor deja-dup ffmpeg-free gcc gcc-c++ gimp gnome-extensions-app gnome-shell-extension-appindicator gnome-shell-extension-auto-move-windows gnome-shell-extension-caffeine gnome-shell-extension-dash-to-dock gnome-tweaks htop java-17-openjdk-* mpv ncdu neofetch nmap nodejs nvtop obs-studio steam yt-dlp -y
 # Multimedia codecs
 sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y
 sudo dnf install lame\* --exclude=lame-devel -y
@@ -24,14 +24,19 @@ sudo dnf group upgrade --with-optional Multimedia -y
 # Non-automated installs
 echo "Manually install these programs:"
 echo "Bitwarden:    https://bitwarden.com/download/"
-echo "Clipboard:    https://extensions.gnome.org/extension/779/clipboard-indicator/"
 echo "Discord:      https://discord.com"
 echo "IntelliJ:     https://www.jetbrains.com/idea/download/?section=linux"
 echo "Minecraft:    https://www.minecraft.net/en-us/download"
 echo "Postman:      https://www.postman.com/downloads/"
 echo "ProtonVPN:    https://protonvpn.com/support/official-linux-vpn-fedora/"
-echo "Vitals:       https://extensions.gnome.org/extension/1460/vitals/"
 echo "VS Code:      https://code.visualstudio.com/Download"
+
+# GNOME Extensions
+echo "Install these GNOME Extensions:"
+echo "Alphabetical App Grid:    https://extensions.gnome.org/extension/4269/alphabetical-app-grid/"
+echo "Clipboard History:        https://extensions.gnome.org/extension/4839/clipboard-history/"
+echo "Extension List:           https://extensions.gnome.org/extension/3088/extension-list/"
+echo "Vitals:                   https://extensions.gnome.org/extension/1460/vitals/"
 
 read -rsp $'Press any key to continue...\n' -n 1 key
 
