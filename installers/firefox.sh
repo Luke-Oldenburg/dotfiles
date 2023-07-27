51a9dea0071960eae50967c5396db372cb04672e
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2023-07-27 02:57:14 -0400

    Add -y to apt

--- installers/firefox.sh
+++ installers/firefox.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo snap remove firefox
-sudo apt remove firefox
-sudo add-apt-repository ppa:mozillateam/ppa
+sudo apt remove firefox -y
+sudo add-apt-repository ppa:mozillateam/ppa -y
 echo '
 Package: *
 Pin: release o=LP-PPA-mozillateam
 Pin-Priority: 1001
 ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
-sudo apt install firefox
+sudo apt install firefox -y
