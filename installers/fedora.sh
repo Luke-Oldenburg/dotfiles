884385b16d47ffbb6eed28caa7f986ebcecb0ed7
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-05-25 21:35:58 -0400

    Swap ffmpeg

--- installers/fedora.sh
+++ installers/fedora.sh
@@ -52,6 +52,7 @@ echo "Installing multimedia codecs."
 sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y
 sudo dnf install lame\* --exclude=lame-devel -y
 sudo dnf group upgrade --with-optional Multimedia -y
+sudo dnf swap ffmpeg-free ffmpeg --allowerasing
 
 # Install flatpaks
 echo "Installing flatpaks."
